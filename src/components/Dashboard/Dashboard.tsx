02464b3a6fd2c873e91af33efcf25a16a2cb906c
NicssLucik02 <nikitalucenko1@gmail.com>
2026-01-10 00:37:59 +0200

    fix build

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,12 +1,9 @@
-import { Leaderboard } from '../Leaderboard/Leaderboard';
 import styles from './dashboard.module.scss';
 
 export const Dashboard = () => {
   return (
     <section className={styles['dashboard']}>
-      <div className={styles['dashboardContainer']}>
-        <Leaderboard />
-      </div>
+      <div className={styles['dashboardContainer']}></div>
     </section>
   );
 };
